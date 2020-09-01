f2bd69b3282c67cb952c014945fe1f0ed1537b6f
João <Joo@UNO91>
2020-09-01 20:32:40 -0300

    alterações 01/09

---
+++ WebVendas/Controllers/DepartmentsControler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using WebVendas.Models;
+
+public class Departments.Controlers
+{
+	Public 1action Result Index()
+
+
+
+        {
+		List<Departments> list = new List<Departments>();
+		list.Add(new Departments { id = 1, name = "Eletronics" });
+		list.Add(new Departments { id = 2, name = "Eles" });
+		list.Add(new Departments { id = 3, name = "Elics" });
+		list.Add(new Departments { id = 4, name = "Eletrs" });
+		return Views();
+    }
+}
+{
+	public View(List);
+	{
+	}
+}
---
+++ WebVendas/Models/Department.cs
@@ -0,0 +1,12 @@
+﻿namespace WebVendas
+
+public class Department
+{
+    public int id { get; set }
+	{
+
+	public string Name { get; set }{
+
+	}
+}
+}
